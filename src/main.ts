1d9941d51affb34e9d760f0217567641e92b1a8f
Pedro da Luz <pedro.luz@hotmail.com.br>
2020-11-08 18:55:11 -0300

    Localização

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
+// @ts-ignore
+import VueMoment from "vue-moment";
+
+Vue.use(VueMoment);
 
 Vue.config.productionTip = false;
 
@@ -11,5 +17,6 @@ new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,52 @@
+/* eslint-disable @typescript-eslint/camelcase */
+process.env.VUE_APP_NAME = require("./package.json").name;
+process.env.VUE_APP_VERSION = require("./package.json").version;
+process.env.VUE_APP_THEME_COLOR = "#ee44aa";
+
 module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.ts",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: process.env.VUE_APP_NAME,
+      color: process.env.VUE_APP_THEME_COLOR,
+      locale: process.env.VUE_APP_I18N_LOCALE || "en",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+  },
+
   lintOnSave: false,
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: process.env.VUE_APP_I18N_LOCALE || "en",
+      fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
+      localeDir: "locales",
+      enableInSFC: true
+    },
+    moment: {
+      locales: [
+        process.env.VUE_APP_I18N_LOCALE || "en",
+        (process.env.VUE_APP_I18N_LOCALE || "en") === "en" &&
+        (process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en") === "en"
+          ? process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en"
+          : process.env.VUE_APP_I18N_FALLBACK_LOCALE
+      ]
+    }
+  },
+
+  pwa: {
+    name: process.env.VUE_APP_NAME,
+    theme_color: process.env.VUE_APP_THEME_COLOR,
+    display: "fullscreen"
+  }
 };
