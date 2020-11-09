bf1801124ce2278f9da64da23763b5666994c0e7
Pedro da Luz <pedro.luz@hotmail.com.br>
2020-11-09 04:22:57 -0300

    Ícones para PWA

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/camelcase */
 process.env.VUE_APP_NAME = require("./package.json").name;
 process.env.VUE_APP_VERSION = require("./package.json").version;
-process.env.VUE_APP_THEME_COLOR = "#ee44aa";
+process.env.VUE_APP_THEME_COLOR = "#ff0000";
 
 module.exports = {
   pages: {
@@ -15,6 +15,7 @@ module.exports = {
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: process.env.VUE_APP_NAME,
+      version: process.env.VUE_APP_VERSION,
       color: process.env.VUE_APP_THEME_COLOR,
       locale: process.env.VUE_APP_I18N_LOCALE || "en",
       // chunks to include on this page, by default includes
@@ -47,6 +48,10 @@ module.exports = {
   pwa: {
     name: process.env.VUE_APP_NAME,
     theme_color: process.env.VUE_APP_THEME_COLOR,
-    display: "fullscreen"
+    display: "fullscreen",
+    msTileColor: "#00aba9",
+    iconPaths: {
+      msTileImage: "img/icons/mstile-150x150.png"
+    }
   }
 };
